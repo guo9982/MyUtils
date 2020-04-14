07dae7920ed20e6cc10d2c510ba6e2fb475aaaa5
guo9982 <guo9982>
2020-04-14 16:41:55 +0800

    Initial commit

---
+++ src/main/java/cn/org/mytest/conf/ConfigurationManager.java
@@ -0,0 +1,46 @@
+package cn.org.mytest.conf;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigurationManager {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConfigurationManager.class);
+
+    private static final Properties properties = new Properties();
+
+    static {
+        logger.info("开始加重配置文件。。。");
+        InputStream in = ConfigurationManager.class.getClassLoader().getResourceAsStream("conf.properties");
+        try {
+            properties.load(in);
+            logger.info("配置文件加重完成！");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取属性。
+     * @param key 属性字段
+     * @param type 返回类型
+     * @return 属性值
+     */
+    public static Object getStringProperty(String key, String type) {
+        String res = properties.getProperty(key, "");
+        if (type.equalsIgnoreCase("int")) {
+            return Integer.valueOf(res);
+        } else if (type.equalsIgnoreCase("double")) {
+            return Double.valueOf(res);
+        } else if (type.equalsIgnoreCase("long")) {
+            return Long.valueOf(res);
+        } else {
+            return res;
+        }
+    }
+
+}
