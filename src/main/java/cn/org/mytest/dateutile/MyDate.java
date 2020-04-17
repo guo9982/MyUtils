e5d336ad1059455a18b5a22bbe737bf7bae20e13
G <guo9982@qq.com>
2020-04-17 17:23:30 +0800

    3，日期工具完善中。。。

--- src/main/java/cn/org/mytest/dateutile/MyDate.java
+++ src/main/java/cn/org/mytest/dateutile/MyDate.java
@@ -4,15 +4,29 @@ import java.time.LocalDate;
 import java.time.Period;
 
 public class MyDate {
-    public static int getDay(int year,int mouth,int day) {
+    public static String getDays(int year,int month,int day) {
         // 获取当前时间
         LocalDate nowDate = LocalDate.now();
         // 构建指定日期
-        LocalDate dirtDate = LocalDate.of(year, mouth, day);
+        LocalDate dirtDate = LocalDate.of(year, month, day);
 
         Period period = Period.between(nowDate, dirtDate);
 
+        int intYear = period.getYears();
+        int intMonth = period.getMonths();
+        int intDay = period.getDays();
+
+        StringBuffer sb;
+        sb = new StringBuffer();
+        sb.append(year).append("年").append(month).append("月").append(day).append("日比今天");
+        if (intYear > 0) {
+            sb.append("晚").append(intYear).append("年").append(intMonth).append("月").append(intDay).append("日");
+        } else if (intYear == 0 && intMonth < 0){
+            sb.append("早").append(intYear).append("年").append(intMonth).append("月").append(intDay).append("日");
+        }
+
+
         // 获取相隔天数
-        return period.getDays();
+        return sb.toString();
     }
 }
