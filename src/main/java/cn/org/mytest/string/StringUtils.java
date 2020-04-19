fc00731cca7871b941f1ab0c40b086c9918a252f
guo9982 <guo9982>
2020-04-19 15:56:24 +0800

    1，字符串工具完善中。。。

--- src/main/java/cn/org/mytest/string/StringUtils.java
+++ src/main/java/cn/org/mytest/string/StringUtils.java
@@ -1,6 +1,14 @@
 package cn.org.mytest.string;
 
+/**
+ * 字符串工具类
+ */
 public class StringUtils {
+    /**
+     * 检查字符串是否为空
+     * @param str
+     * @return
+     */
     public static boolean isEmpty(String str) {
         return str == null || "".equalsIgnoreCase(str);
     }
