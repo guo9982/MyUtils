2c4acca2cc37248a9c819c8f27414012a83c402b
G <guo9982@qq.com>
2020-04-23 10:01:37 +0800

    1，日期工具完善中。。。

--- src/test/java/cn/org/mytest/Test1.java
+++ src/test/java/cn/org/mytest/Test1.java
@@ -3,6 +3,8 @@ package cn.org.mytest;
 import cn.org.mytest.dateutile.DateUtils;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 public class Test1 {
 
     @Test
@@ -15,5 +17,10 @@ public class Test1 {
     public void test2() {
         System.out.println(DateUtils.getTodayDate());
 
+    }
+
+    @Test
+    public void test3() {
+        System.out.println(DateUtils.getDays("2020-04-04", "2020-04-05"));
     }
 }
