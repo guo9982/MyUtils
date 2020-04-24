f17050f2ff8038c925b45106de7f1fd5926ade08
guo9982 <guo9982>
2020-04-24 10:23:05 +0800

    1，代码整理。

--- src/main/java/cn/org/mytest/string/StringUtils.java
+++ src/main/java/cn/org/mytest/string/StringUtils.java
@@ -4,14 +4,7 @@ package cn.org.mytest.string;
  * 字符串工具类
  */
 public class StringUtils {
-    /**
-     * 检查字符串是否为空
-     * @param str
-     * @return
-     */
-    public static boolean isEmpty(String str) {
-        return str == null || "".equalsIgnoreCase(str);
-    }
+
 
     /**
      * 判断字符串是否不为空。
