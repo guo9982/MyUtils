9af2a9a0753f9ab9999096188ce2976354fd63b6
guo9982 <guo9982>
2020-04-24 17:58:42 +0800

    2，方法修改。

--- src/main/java/cn/org/mytest/string/StringUtils.java
+++ src/main/java/cn/org/mytest/string/StringUtils.java
@@ -15,5 +15,27 @@ public class StringUtils {
         return str == null || "".equalsIgnoreCase(str);
     }
 
+    /**
+     * 判断字符串是否不为空。
+     * @param str
+     * @return
+     */
+    public static boolean isNotEmpty(String str) {
+        return str != null && !"".equals(str);
+    }
+    /**
+     * 截断字符串两侧的逗号
+     * @param str 字符串
+     * @return 字符串
+     */
+    public static String trimComma(String str) {
+        if(str.startsWith(",")) {
+            str = str.substring(1);
+        }
+        if(str.endsWith(",")) {
+            str = str.substring(0, str.length() - 1);
+        }
+        return str;
+    }
 
 }
