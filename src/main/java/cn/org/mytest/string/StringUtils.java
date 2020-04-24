3a7cc7fd9675b63804b6aecb2e1c8489d23a1491
guo9982 <guo9982>
2020-04-24 10:24:14 +0800

    1，代码整理。

--- src/main/java/cn/org/mytest/string/StringUtils.java
+++ src/main/java/cn/org/mytest/string/StringUtils.java
@@ -7,41 +7,5 @@ public class StringUtils {
 
 
 
-    /**
-     * 从拼接的字符串中给字段设置值
-     * @param str 字符串
-     * @param delimiter 分隔符
-     * @param field 字段名
-     * @param newFieldValue 新的field值
-     * @return 字段值
-     *  name=zhangsan|age=12
-     */
-    public static String setFieldInConcatString(String str,
-                                                String delimiter,
-                                                String field,
-                                                String newFieldValue) {
 
-        // searchKeywords=iphone7|clickCategoryIds=1,2,3
-
-        String[] fields = str.split(delimiter);
-
-        for(int i = 0; i < fields.length; i++) {
-            String fieldName = fields[i].split("=")[0];
-            if(fieldName.equals(field)) {
-                String concatField = fieldName + "=" + newFieldValue;
-                fields[i] = concatField;
-                break;
-            }
-        }
-
-        StringBuilder buffer = new StringBuilder();
-        for(int i = 0; i < fields.length; i++) {
-            buffer.append(fields[i]);
-            if(i < fields.length - 1) {
-                buffer.append("|");
-            }
-        }
-
-        return buffer.toString();
-    }
 }
