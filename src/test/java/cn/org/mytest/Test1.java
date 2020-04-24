5b44fa13ce64e1b60303f2dbb5b1517b5afbe1a8
G <guo9982@qq.com>
2020-04-24 10:22:29 +0800

    1，代码整理。

--- src/test/java/cn/org/mytest/Test1.java
+++ src/test/java/cn/org/mytest/Test1.java
@@ -7,20 +7,4 @@ import java.time.LocalDate;
 
 public class Test1 {
 
-    @Test
-    public void test1() {
-
-        System.out.println(DateUtils.getDays(2020, 12, 1));
-    }
-
-    @Test
-    public void test2() {
-        System.out.println(DateUtils.getTodayDate());
-
-    }
-
-    @Test
-    public void test3() {
-        System.out.println(DateUtils.getDays("2020-04-04", "2020-04-05"));
-    }
 }
