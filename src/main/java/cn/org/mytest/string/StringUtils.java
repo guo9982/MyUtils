5ba05c2449c6575fec22300873db64039b0b913c
G <guo9982@qq.com>
2020-04-24 17:57:32 +0800

    1，代码整理。

--- src/main/java/cn/org/mytest/string/StringUtils.java
+++ src/main/java/cn/org/mytest/string/StringUtils.java
@@ -6,6 +6,14 @@ package cn.org.mytest.string;
 public class StringUtils {
 
 
+    /**
+     * 检查字符串是否为空
+     * @param str
+     * @return
+     */
+    public static boolean isEmpty(String str) {
+        return str == null || "".equalsIgnoreCase(str);
+    }
 
 
 }
