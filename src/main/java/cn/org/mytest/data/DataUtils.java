d782b71d211d078975a3ab32c828d391d7decc75
guo9982 <guo9982>
2020-04-25 10:08:17 +0800

    2，虚拟数据。

---
+++ src/main/java/cn/org/mytest/data/DataUtils.java
@@ -0,0 +1,7 @@
+package cn.org.mytest.data;
+
+public class DataUtils {
+    public static void moke() {
+        String[] locations = new String[]{"鲁","京","粤","京","沪","京","京","深","京","京"};
+    }
+}
