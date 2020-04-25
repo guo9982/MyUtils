d065193c7674752b0e3c7a1fe2ea97e8acc5b07f
G <guo9982@qq.com>
2020-04-25 10:24:49 +0800

    2，虚拟数据。

--- src/main/java/cn/org/mytest/data/DataUtils.java
+++ src/main/java/cn/org/mytest/data/DataUtils.java
@@ -1,7 +1,12 @@
 package cn.org.mytest.data;
 
+import cn.org.mytest.dateutile.DateUtils;
+
+import java.util.Random;
+
 public class DataUtils {
     public static void moke() {
+        Random random = new Random();
         // 大陆地区
         String[] locations = new String[]
                 {"京", "津", "渝", "沪",
@@ -11,5 +16,7 @@ public class DataUtils {
                         "闽", "赣", "鲁", "豫", "鄂", "湘",
                         "粤", "琼", "川", "黔", "云",
                         "陕", "甘", "青", "内蒙古", "桂", "宁", "新", "藏"};
+        // 获取今天日期
+        String todayDate = DateUtils.getTodayDate();
     }
 }
