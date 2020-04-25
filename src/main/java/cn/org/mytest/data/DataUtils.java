392506f8232f2cf3ecb153e145f2793371fbfec3
G <guo9982@qq.com>
2020-04-25 10:23:03 +0800

    2，虚拟数据。

--- src/main/java/cn/org/mytest/data/DataUtils.java
+++ src/main/java/cn/org/mytest/data/DataUtils.java
@@ -2,6 +2,14 @@ package cn.org.mytest.data;
 
 public class DataUtils {
     public static void moke() {
-        String[] locations = new String[]{"鲁","京","粤","京","沪","京","京","深","京","京"};
+        // 大陆地区
+        String[] locations = new String[]
+                {"京", "津", "渝", "沪",
+                        "冀", "晋",
+                        "辽", "吉", "黑",
+                        "苏", "浙", "皖",
+                        "闽", "赣", "鲁", "豫", "鄂", "湘",
+                        "粤", "琼", "川", "黔", "云",
+                        "陕", "甘", "青", "内蒙古", "桂", "宁", "新", "藏"};
     }
 }
