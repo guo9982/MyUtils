f1b5541e052ec77bfeb780827d2e78faeac297d4
G <guo9982@qq.com>
2020-04-27 15:03:27 +0800

    1，虚拟数据。

--- src/test/java/cn/org/mytest/Test1.java
+++ src/test/java/cn/org/mytest/Test1.java
@@ -1,11 +1,5 @@
 package cn.org.mytest;
 
-import cn.org.mytest.data.DataUtils;
-import org.junit.Test;
-
 public class Test1 {
-    @Test
-    public void test1() {
-        DataUtils.moke(10);
-    }
+
 }
