711a8579f81da637d49920adf1d8d6ca29bd6236
G <guo9982@qq.com>
2020-04-29 11:22:21 +0800

    1，文件操作相关工具。

--- src/main/java/cn/org/mytest/file/FileUtils.java
+++ src/main/java/cn/org/mytest/file/FileUtils.java
@@ -4,15 +4,8 @@ import java.io.File;
 
 public class FileUtils {
     public static String copyFile(String src,String dsrc) {
-        String result = "";
-        File srcFile = new File(src);
-        if (srcFile.exists()) {
-            if (srcFile.isFile()) {
-
-            }
-        } else {
-            result = "源文件不存在！";
-        }
+        String result;
+        result = "";
 
         return result;
     }
