c36f7e316a73469b1d43c9dab8278163e35c68ed
guo9982 <guo9982>
2020-04-30 15:04:48 +0800

    1，文件操作相关工具。

--- src/main/java/cn/org/mytest/file/FileUtils.java
+++ src/main/java/cn/org/mytest/file/FileUtils.java
@@ -1,11 +1,21 @@
 package cn.org.mytest.file;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 
 public class FileUtils {
-    public static String copyFile(String src,String dsrc) {
-        String result;
-        result = "";
+    public static String copyFile(String srcPathStr,String desPathStr) throws IOException {
+        String result = "error";
+        File srcFile = new File(srcPathStr);
+        if (!srcFile.exists()) {
+            result = "源文件不存在";
+        } else {
+            FileChannel.open(Paths.get(srcPathStr), StandardOpenOption.READ);
+        }
 
         return result;
     }
