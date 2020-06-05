9dc7950a94cc82e98443b6ede344a79d9bf33b83
guo9982 <guo9982>
2020-06-05 09:20:00 +0800

    1，文件操作工具代码整理。

--- src/main/java/cn/org/mytest/file/FileUtils.java
+++ src/main/java/cn/org/mytest/file/FileUtils.java
@@ -8,7 +8,7 @@ import java.nio.file.StandardOpenOption;
 
 
 public class FileUtils {
-    public static String copyFile5(String srcPathStr,String desPathStr) throws IOException {
+    public static String copyFile6(String srcPathStr,String desPathStr) throws IOException {
         String result;
         File srcFile = new File(srcPathStr);
         if (!srcFile.exists()) {
