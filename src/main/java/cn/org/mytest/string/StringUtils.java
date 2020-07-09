993dabb9ca1836a2f7320bc2d93a8f13ace682cd
G <guo9982@qq.com>
2020-07-09 08:50:37 +0800

    1，字符串工具修改。

--- src/main/java/cn/org/mytest/string/StringUtils.java
+++ src/main/java/cn/org/mytest/string/StringUtils.java
@@ -13,7 +13,7 @@ public class StringUtils {
      * @param str
      * @return
      */
-    public static boolean isEmpty46(String str) {
+    public static boolean isEmpty6(String str) {
         return str == null || "".equalsIgnoreCase(str);
     }
 
