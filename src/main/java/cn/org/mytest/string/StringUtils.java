dbdb90f215a12f7d295efdfc35e959049ed3b8d1
G <guo9982@qq.com>
2020-07-31 09:41:24 +0800

    字符工具更新。

--- src/main/java/cn/org/mytest/string/StringUtils.java
+++ src/main/java/cn/org/mytest/string/StringUtils.java
@@ -13,7 +13,7 @@ public class StringUtils {
      * @param str
      * @return
      */
-    public static boolean isEmpty113(String str) {
+    public static boolean isEmpty12(String str) {
         return str == null || "".equalsIgnoreCase(str);
     }
 
