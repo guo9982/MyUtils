08f0e4d260097b829b5565853bdadab48d667940
guo9982 <guo9982>
2020-08-06 08:51:06 +0800

    字符串工具更新。

--- src/main/java/cn/org/mytest/string/StringUtils.java
+++ src/main/java/cn/org/mytest/string/StringUtils.java
@@ -13,7 +13,7 @@ public class StringUtils {
      * @param str
      * @return
      */
-    public static boolean isEmpty45(String str) {
+    public static boolean isEmpty(String str) {
         return str == null || "".equalsIgnoreCase(str);
     }
 
