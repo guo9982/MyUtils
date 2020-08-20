d98916adb29083c184aae58671d2a30e70c1fd16
guo9982 <guo9982>
2020-08-20 09:07:12 +0800

    字符工具更新。

--- src/main/java/cn/org/mytest/string/StringUtils.java
+++ src/main/java/cn/org/mytest/string/StringUtils.java
@@ -13,7 +13,7 @@ public class StringUtils {
      * @param str
      * @return
      */
-    public static boolean isEmpty18(String str) {
+    public static boolean isEmpty19(String str) {
         return str == null || "".equalsIgnoreCase(str);
     }
 
