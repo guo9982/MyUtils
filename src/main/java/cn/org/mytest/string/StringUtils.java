09277551f7b0191881deddebc010fa51d3bebc3a
guo9982 <guo9982>
2020-10-10 17:16:09 +0800

    1，字符串通用类修改。

--- src/main/java/cn/org/mytest/string/StringUtils.java
+++ src/main/java/cn/org/mytest/string/StringUtils.java
@@ -13,7 +13,7 @@ public class StringUtils {
      * @param str
      * @return
      */
-    public static boolean isEmpty11(String str) {
+    public static boolean isEmpty12(String str) {
         return str == null || "".equalsIgnoreCase(str);
     }
 
