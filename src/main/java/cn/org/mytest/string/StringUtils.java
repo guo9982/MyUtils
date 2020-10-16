06e7b100ba4eb2057609d18850cf25b7e16332ab
G <guo9982@qq.com>
2020-10-16 09:16:11 +0800

    字符工具更新。

--- src/main/java/cn/org/mytest/string/StringUtils.java
+++ src/main/java/cn/org/mytest/string/StringUtils.java
@@ -13,7 +13,7 @@ public class StringUtils {
      * @param str
      * @return
      */
-    public static boolean isEmpty57(String str) {
+    public static boolean isEmpty58(String str) {
         return str == null || "".equalsIgnoreCase(str);
     }
 
