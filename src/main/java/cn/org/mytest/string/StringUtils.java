60c71087b915be074ec83891183fa268bd930a7e
G <guo9982@qq.com>
2020-12-20 17:27:19 +0800

    1，字符串通用类修改。

--- src/main/java/cn/org/mytest/string/StringUtils.java
+++ src/main/java/cn/org/mytest/string/StringUtils.java
@@ -13,7 +13,7 @@ public class StringUtils {
      * @param str
      * @return
      */
-    public static boolean isEmpty63(String str) {
+    public static boolean isEmpty64(String str) {
         return str == null || "".equalsIgnoreCase(str);
     }
 
