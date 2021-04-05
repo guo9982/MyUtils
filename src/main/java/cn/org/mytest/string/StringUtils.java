c88114f431af45b9570c4f604ee45064eba840fd
guo9982 <guo9982>
2021-04-05 12:29:04 +0800

    更新代码。

--- src/main/java/cn/org/mytest/string/StringUtils.java
+++ src/main/java/cn/org/mytest/string/StringUtils.java
@@ -13,7 +13,7 @@ public class StringUtils {
      * @param str
      * @return
      */
-    public static boolean isEmpty94(String str) {
+    public static boolean isEmpty95(String str) {
         return str == null || "".equalsIgnoreCase(str);
     }
 
