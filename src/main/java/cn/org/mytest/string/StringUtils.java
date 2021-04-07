4e2b7fc0ea841211f3fae55a829a3a607d598ba1
guo9982 <guo9982@qq.com>
2021-04-07 12:29:34 +0800

    更新代码。

--- src/main/java/cn/org/mytest/string/StringUtils.java
+++ src/main/java/cn/org/mytest/string/StringUtils.java
@@ -13,7 +13,7 @@ public class StringUtils {
      * @param str
      * @return
      */
-    public static boolean isEmpty97(String str) {
+    public static boolean isEmpty98(String str) {
         return str == null || "".equalsIgnoreCase(str);
     }
 
