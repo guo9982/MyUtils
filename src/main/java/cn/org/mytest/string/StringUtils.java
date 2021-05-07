e98038a54e5a29b35c9a043fbeaeea1739f276fb
guo9982 <guo9982@qq.com>
2021-05-07 16:36:11 +0800

    update code

--- src/main/java/cn/org/mytest/string/StringUtils.java
+++ src/main/java/cn/org/mytest/string/StringUtils.java
@@ -13,7 +13,7 @@ public class StringUtils {
      * @param str
      * @return
      */
-    public static boolean isEmpty28(String str) {
+    public static boolean isEmpty29(String str) {
         return str == null || "".equalsIgnoreCase(str);
     }
 
