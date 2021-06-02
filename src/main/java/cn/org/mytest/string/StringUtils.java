0dbd22237ab0a2b77e86e3d06fd5cce212c1150c
guo9982 <guo9982>
2021-06-02 14:49:00 +0800

    update code

--- src/main/java/cn/org/mytest/string/StringUtils.java
+++ src/main/java/cn/org/mytest/string/StringUtils.java
@@ -13,7 +13,7 @@ public class StringUtils {
      * @param str
      * @return
      */
-    public static boolean isEmpty86(String str) {
+    public static boolean isEmpty87(String str) {
         return str == null || "".equalsIgnoreCase(str);
     }
 
