f2747a0054696c86eca416c7bed3f7390d4c498a
guo9982 <guo9982>
2021-06-08 09:02:16 +0800

    update code

--- src/main/java/cn/org/mytest/string/StringUtils.java
+++ src/main/java/cn/org/mytest/string/StringUtils.java
@@ -18,7 +18,7 @@ public class StringUtils {
      * @param str
      * @return
      */
-    public static boolean isEmpty8(String str) {
+    public static boolean isEmpty9(String str) {
         return str == null || "".equalsIgnoreCase(str);
     }
 
