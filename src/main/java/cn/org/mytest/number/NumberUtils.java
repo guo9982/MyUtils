daa3d23de1f0eb287a3d90be9750c7a9689793a0
guo9982 <guo9982>
2022-05-03 14:18:30 +0800

    1，数值工具完善。

---
+++ src/main/java/cn/org/mytest/number/NumberUtils.java
@@ -0,0 +1,17 @@
+package cn.org.mytest.number;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class NumberUtils {
+    /**
+     * 格式化小数
+     * @param num 字符串?
+     * @param scale 四舍五入的位数
+     * @return 格式化小数
+     */
+    public static double formatDouble(double num, int scale) {
+        BigDecimal bd = new BigDecimal(num);
+        return bd.setScale(scale, RoundingMode.HALF_UP).doubleValue();
+    }
+}
