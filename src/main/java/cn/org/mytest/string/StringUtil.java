bc18a9d1b31920692a00539cc97f7989eb2b653e
TOM <guo9982@qq.com>
2022-05-19 10:56:27 +0800

    update code

--- src/main/java/cn/org/mytest/string/StringUtil.java
+++ src/main/java/cn/org/mytest/string/StringUtil.java
@@ -13,7 +13,7 @@ public  class StringUtil {
      * @param str
      * @return
      */
-    public static boolean isEmpty54(String str) {
+    public static boolean isEmpty55(String str) {
         return str == null || "".equalsIgnoreCase(str);
     }
 
