10171733e80e5ddf069ad835a0681a153f89e4f4
CHAO <guo9982@qq.com>
2022-06-01 14:15:39 +0800

    1，日期工具完善。

--- src/main/java/cn/org/mytest/dateutile/DateUtils.java
+++ src/main/java/cn/org/mytest/dateutile/DateUtils.java
@@ -3,12 +3,33 @@ package cn.org.mytest.dateutile;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Date;
 
 public class DateUtils {
 
     public static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
     public static final SimpleDateFormat DATEKEY_FORMAT = new SimpleDateFormat("yyyyMMdd");
+
+    /**
+     * 判断一个时间是否在另一个时间之前
+     * @param time1 第一个时间
+     * @param time2 第二个时间
+     * @return 判断结果
+     */
+    public static boolean before(String time1, String time2) {
+        try {
+            Date dateTime1 = TIME_FORMAT.parse(time1);
+            Date dateTime2 = TIME_FORMAT.parse(time2);
+
+            if(dateTime1.before(dateTime2)) {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
     /**
      * 获取两个日期之间的时间差
