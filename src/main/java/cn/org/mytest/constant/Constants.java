06119f752ff6983eedc5e546c16499e694026d0a
CHAO <guo9982@qq.com>
2022-07-19 16:06:18 +0800

    1，更新常量接口。

--- src/main/java/cn/org/mytest/constant/Constants.java
+++ src/main/java/cn/org/mytest/constant/Constants.java
@@ -12,6 +12,14 @@ public interface Constants {
 
     String SPARK_LOCAL = "spark.local";
 
+    String SPARK_LOCAL_TASKID_MONITOR = "spark.local.taskId.monitorFlow";
+    String SPARK_LOCAL_TASKID_EXTRACT_CAR= "spark.local.taskId.extractCar";
+    String SPARK_LOCAL_WITH_THE_CAR = "spark.local.taskId.withTheCar";
+    String SPARK_LOCAL_TASKID_TOPN_MONITOR_FLOW = "spark.local.taskid.tpn.road.flow";
+    String SPARK_LOCAL_TASKID_MONITOR_ONE_STEP_CONVERT = "spark.local.taskid.road.one.step.convert";
+    String KAFKA_METADATA_BROKER_LIST = "kafka.metadata.broker.list";
+    String KAFKA_TOPICS = "kafka.topics";
+
     // 增加数据库类型
     String DB_TYPE = "DB_TYPE";
 
