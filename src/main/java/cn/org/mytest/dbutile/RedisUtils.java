50ef36fe788eee6cd349b587bb9e30323521b408
chao <guo9982@qq.com>
2023-04-08 22:33:52 +0800

    1，添加Redis相关支持代码。

--- src/main/java/cn/org/mytest/dbutile/RedisUtils.java
+++ src/main/java/cn/org/mytest/dbutile/RedisUtils.java
@@ -1,4 +1,10 @@
 package cn.org.mytest.dbutile;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class RedisUtils {
+    private static final Logger logger = LoggerFactory.getLogger(RedisUtils.class);
+
 }
