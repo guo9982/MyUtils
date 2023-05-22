40dfdbe1f1500493e694504344823d3d42272dbf
guo9982 <guo9982>
2023-05-22 17:41:56 +0800

    1，代码更新。

--- src/main/java/cn/org/mytest/utils/DateUtils.java
+++ src/main/java/cn/org/mytest/utils/DateUtils.java
@@ -1,5 +1,6 @@
 package cn.org.mytest.utils;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Period;
@@ -8,9 +9,12 @@ import java.util.Date;
 
 public class DateUtils {
 
-    public static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
-    public static final SimpleDateFormat DATEKEY_FORMAT = new SimpleDateFormat("yyyyMMdd");
+    public static final SimpleDateFormat TIME_FORMAT =
+            new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    public static final SimpleDateFormat DATE_FORMAT =
+            new SimpleDateFormat("yyyy-MM-dd");
+    public static final SimpleDateFormat DATEKEY_FORMAT =
+            new SimpleDateFormat("yyyyMMdd");
 
     /**
      * 判断一个时间是否在另一个时间之前
@@ -34,60 +38,67 @@ public class DateUtils {
     }
 
     /**
-     * 获取两个日期之间的时间差
+     * 判断一个时间是否在另一个时间之后
      *
-     * @param year  年份
-     * @param month 月份
-     * @param day   天
-     * @return
+     * @param time1 第一个时间
+     * @param time2 第二个时间
+     * @return 判断结果
      */
-    public static String getDays(int year, int month, int day) {
-        // 获取当前时间
-        LocalDate nowDate = LocalDate.now();
-        // 构建指定日期
-        LocalDate dirtDate = LocalDate.of(year, month, day);
+    public static boolean after(String time1, String time2) {
+        try {
+            Date dateTime1 = TIME_FORMAT.parse(time1);
+            Date dateTime2 = TIME_FORMAT.parse(time2);
 
-        Period period = Period.between(nowDate, dirtDate);
-
-        int intYear = period.getYears();
-        int intMonth = period.getMonths();
-        int intDay = period.getDays();
-
-        StringBuilder sb;
-        sb = new StringBuilder();
-        sb.append(year).append("年").append(month).append("月").append(day).append("日比今天");
-        if (intYear > 0 || (intYear == 0 && intMonth > 0) || (intYear == 0 && intMonth == 0 && intDay > 0)) {
-            sb.append("晚");
-        } else {
-            sb.append("早");
+            if (dateTime1.after(dateTime2)) {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        sb.append(intYear).append("年").append(intMonth).append("月").append(intDay).append("日");
-        // 获取相隔天数
-        return sb.toString();
+        return false;
     }
 
     /**
-     * 计算两个日期之间相差的天数
+     * 计算时间差值（单位为秒）
      *
-     * @param startDate 开始日期 格式为 2020-04-01
-     * @param endDate   结束日期 2020-04-01
-     * @return Long 两个时间之间的间隔天数
+     * @param time1 时间1
+     * @param time2 时间2
+     * @return 差值
      */
-    public static Long getDays(String startDate, String endDate) {
-        LocalDate start = LocalDate.parse(startDate);
-        LocalDate end = LocalDate.parse(endDate);
+    public static int minus(String time1, String time2) {
+        try {
+            Date datetime1 = TIME_FORMAT.parse(time1);
+            Date datetime2 = TIME_FORMAT.parse(time2);
 
-        return Math.abs(start.toEpochDay() - end.toEpochDay());
+            long millisecond = datetime1.getTime() - datetime2.getTime();
+
+            return Integer.valueOf(String.valueOf(millisecond / 1000));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     /**
-     * 获取当天日期
+     * 获取年月日和小时
      *
-     * @return
+     * @param datetime 时间（yyyy-MM-dd HH:mm:ss）
+     * @return 结果（yyyy-MM-dd_HH）
+     */
+    public static String getDateHour(String datetime) {
+        String date = datetime.split(" ")[0];
+        String hourMinuteSecond = datetime.split(" ")[1];
+        String hour = hourMinuteSecond.split(":")[0];
+        return date + "_" + hour;
+    }
+
+    /**
+     * 获取当天日期（yyyy-MM-dd）
+     *
+     * @return 当天日期
      */
     public static String getTodayDate() {
-        return String.valueOf(LocalDate.now());
+        return DATE_FORMAT.format(new Date());
     }
 
     /**
@@ -123,5 +134,71 @@ public class DateUtils {
      */
     public static String formatTime(Date date) {
         return TIME_FORMAT.format(date);
+    }
+
+    /**
+     * 解析时间字符串
+     *
+     * @param time 时间字符串
+     * @return Date
+     */
+    public static Date parseTime(String time) {
+        try {
+            return TIME_FORMAT.parse(time);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 格式化日期key
+     *
+     * @param date
+     * @return yyyyMMdd
+     */
+    public static String formatDateKey(Date date) {
+        return DATEKEY_FORMAT.format(date);
+    }
+
+    /**
+     * 格式化日期key	(yyyyMMdd)
+     *
+     * @param datekey
+     * @return
+     */
+    public static Date parseDateKey(String datekey) {
+        try {
+            return DATEKEY_FORMAT.parse(datekey);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 格式化时间，保留到分钟级别
+     *
+     * @param date
+     * @return yyyyMMddHHmm --201701012301
+     */
+    public static String formatTimeMinute(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmm");
+        return sdf.format(date);
+    }
+
+    /**
+     * @param dateTime yyyy-MM-dd HH:mm:ss
+     * @return
+     */
+    public static String getRangeTime(String dateTime) {
+        String date = dateTime.split(" ")[0];
+        String hour = dateTime.split(" ")[1].split(":")[0];
+        int minute = StringUtils.convertStringtoInt(dateTime.split(" ")[1].split(":")[1]);
+//		 String second = dateTime.split(" ")[1].split(":")[2];
+        if (minute + (5 - minute % 5) == 60) {
+            return date + " " + hour + ":" + StringUtils.fulfuill((minute - (minute % 5)) + "") + "~" + date + " " + StringUtils.fulfuill((Integer.parseInt(hour) + 1) + "") + ":00";
+        }
+        return date + " " + hour + ":" + StringUtils.fulfuill((minute - (minute % 5)) + "") + "~" + date + " " + hour + ":" + StringUtils.fulfuill((minute + (5 - minute % 5)) + "");
     }
 }
