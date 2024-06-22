03e9b219381682e76462baca26d86eba7ae3435a
guo9982 <guo9982>
2024-06-22 21:20:26 +0800

    增加功能代码。

--- src/main/java/com/mujie/spark/dao/IWithTheCarDAO.java
+++
@@ -1,5 +0,0 @@
-package com.mujie.spark.dao;
-
-public interface IWithTheCarDAO {
-	void updateTestData(String param);
-}
--- src/main/java/com/producedate2hive/test.java
+++
@@ -1,7 +0,0 @@
-package com.producedate2hive;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println(""!=null);
-    }
-}
